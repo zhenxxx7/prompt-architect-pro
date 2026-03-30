2628d67cafc9043353d4bd9eced2892f8c9b6642
Misael Jordyy <zhenxiang@outlook.sg>
2026-03-30 09:02:45 +0700

    Initial commit from Blink

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BlinkUIProvider, Toaster } from '@blinkdotnew/ui'
+import App from './App'
+import './index.css'
+
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BlinkUIProvider theme="linear" darkMode="system">
+        <Toaster />
+        <App />
+      </BlinkUIProvider>
+    </QueryClientProvider>
+  </React.StrictMode>,
+)
